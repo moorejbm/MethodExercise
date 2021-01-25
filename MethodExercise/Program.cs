ac8d0445a6894674be64ee6129dce852ff1880da
Jason Moore <moorejbm@gmail.com>
2021-01-25 11:33:12 -0600

    MethodExercise

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------------Exercise 1 
+            // Name: Michael
+            // Favorite Color: Blue
+            // Favorite Animal: Walrus
+            // Favorite Band: The Beatles
+
+            Console.WriteLine(" Hello what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($" {color} is an awsom color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+        }
+    }
+}
